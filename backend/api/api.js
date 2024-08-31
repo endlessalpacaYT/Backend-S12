d070ef1a36da6e162d0bd5bc37c27125636de46e
OptiX YT <149933000+optixyt0@users.noreply.github.com>
2024-08-31 18:04:21 +0100

    Fixed login sht

--- backend/api/api.js
+++ backend/api/api.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const User = require('../Models/user.js)
 
 const app = express();
 const verboseLogging = process.env.VERBOSE_LOGGING;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/version', (req, res) => {
     res.send({ version: '0.1'});
@@ -9,3 +13,33 @@ app.get('/version', (req, res) => {
         console.log('Request made to /version');
     }
   });
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ success: false, message: 'Email and Password are required.' });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({ success: false, message: 'Invalid Email.' });
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if (match) {
+            res.json({ success: true, message: 'Login successful', username: user.username, discordId });
+        } else {
+            res.status(401).json({ success: false, message: 'Invalid Password.' });
+        }
+    } catch (error) {
+        if (verboseLogging == "true") {
+            console.error('Error during login of user ', user.username, ':', error);
+        }
+        
+        res.status(500).json({ success: false, message: 'Internal Server Error.' });
+    }
+});
