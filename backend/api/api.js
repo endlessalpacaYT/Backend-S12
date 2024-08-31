eb439ff40a47cc7c0a929ddb4e1e1dbbaa66e449
OptiX YT <149933000+optixyt0@users.noreply.github.com>
2024-08-31 17:35:11 +0100

    Update api.js to finish version stuff

--- backend/api/api.js
+++ backend/api/api.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
 const app = express();
+const verboseLogging = process.env.VERBOSE_LOGGING;
 
 app.get('/version', (req, res) => {
-    res.send()
+    res.send({ version: '0.1'});
+    if (verboseLogging == "true") {
+        console.log('Request made to /version');
+    }
   });
