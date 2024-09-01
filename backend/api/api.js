e6acfd53314637dc15d133094fe2d91d81176eb8
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2024-09-01 01:13:11 +0100

    well unfortunately i forgot to update the version AHHHHH

--- backend/api/api.js
+++ backend/api/api.js
@@ -9,7 +9,7 @@ console.log(chalk.keyword("orange")("[API] ") + "api started!")
 
 app.get('/version', (req, res) => {
     res.send({
-            version: '0.12',
+            version: '0.13',
             versionDate: '01/09/2024',
             backend: 'Backend S12',
             environment: process.env.ENVIRONMENT || "Prod",
